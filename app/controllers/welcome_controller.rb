cb40c03506ee378ef1eb7cda0dbe68c35d4a488a
Fabio Ribeiro <rybeiro@gmail.com>
2019-07-02 17:51:28 -0300

    Aplicando o conhecimento adquirido com Hash, Environment, Variáveis de instância, Query params, Partial templete, Rails console

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,8 @@
 class WelcomeController < ApplicationController
   def index
+    # Para exibir as variaveis de instancia na welcome#index os paramentros
+    # deve ser passado pela url no formato ?nome=valor&curso=valor
+    @nome =  params[:nome].nil? ? "Anonimo" : params[:nome]
+    @curso= params[:curso].nil? ? "Não encontrado" : params[:curso]
   end
 end
