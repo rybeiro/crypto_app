df643f2d492af300df718e04f536ecd3115b00ae
Fabio Ribeiro <rybeiro@gmail.com>
2019-07-09 13:52:18 -0300

    implementando um cookies e uma session

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,9 @@
 class WelcomeController < ApplicationController
   def index
+    # COOKIES
+    cookies[:curso] = "Ruby on Rails"
+    # SESSION
+    session[:user] = "Fulano de tal"
     # Para exibir as variaveis de instancia na welcome#index os paramentros
     # deve ser passado pela url no formato ?nome=valor&curso=valor
     @nome =  params[:nome].nil? ? "Anonimo" : params[:nome]
